1fcf1c9df744e458a87a4f709831d668af8f9111
benjaminderek <benjaminhong@s-169-232-68-207.resnet.ucla.edu>
2015-04-24 22:58:20 -0700

    Test

---
+++ Ben/DataFest.R
@@ -0,0 +1,14 @@
+## For configuration ##
+c("f_date")
+
+
+## For leads ##
+c("dealer_location_id", "lead_date", "price_promise_flag", "dealer_state", "dealer_dma", "dealer_zip", "dealer_distance")
+
+
+## For shopping ##
+c("date")
+
+
+## For transactions ##
+c("date_sold", "lead_date", "dma_bought", "zip_bought", "city_bought", "dealer_location_id")
